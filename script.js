099c4d1387a9e9c2dc19eeb7df9676dd9d448505
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-12 09:48:51 +0300

    It is limited. But it works on old devices!

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var close = document.getElementById('close');
 var subredditName = '';
 var hotImages = [];
 var topImages = [];
+var images = [];
 var afterTop = '';
 var afterHot = '';
 
@@ -64,57 +65,30 @@ function chooseSubreddit() {
 }
 
 function fetchImages(subredditName) {
+  // Promise Hot
+  // var promise = new Promise(function(resolve, reject) {
+  //   var xhrHot = new XMLHttpRequest();
+  //   xhrHot.open(
+  //     'GET',
+  //     `https://www.reddit.com/r/${subredditName}.json?limit=100`
+  //   );
+  //   xhrHot.responseType = 'json';
+  //   xhrHot.send();
+  // })
+
   // Sort by Hot
   var xhrHot = new XMLHttpRequest();
-  xhrHot.open(
-    'GET',
-    `https://www.reddit.com/r/${subredditName}.json?limit=100`
-  );
+  xhrHot.open('GET', `https://www.reddit.com/r/${subredditName}.json`);
   xhrHot.responseType = 'json';
   xhrHot.send();
   xhrHot.onload = function () {
-    // console.log(xhrHot.response);
     var responseObj = xhrHot.response.data.children;
     afterHot = xhrHot.response.data.after;
 
     responseObj.forEach(function (post) {
       // console.log(post.data.preview);
-      if (
-        post.data.preview &&
-        !post.data.is_video
-        // &&
-        // post.data.preview.images[0].resolutions[0].height /
-        //   post.data.preview.images[0].resolutions[0].width >
-        //   1.3
-      ) {
-        hotImages.push(
-          post.data.preview.images[0].resolutions[3].url.replace(/amp;/gi, '')
-        );
-      }
-    });
-  };
-
-  // Sort by Top
-  var xhrTop = new XMLHttpRequest();
-  xhrTop.open(
-    'GET',
-    `https://www.reddit.com/r/${subredditName}/top.json?t=all&limit=100`
-  );
-  xhrTop.responseType = 'json';
-  xhrTop.send();
-  xhrTop.onload = function () {
-    // console.log(xhrTop.response);
-    var responseObj = xhrTop.response.data.children;
-    afterTop = xhrHot.response.data.after;
-    responseObj.forEach(function (post) {
-      if (
-        post.data.preview &&
-        !post.data.is_video
-        // && post.data.preview.images[0].resolutions[0].height /
-        //   post.data.preview.images[0].resolutions[0].width >
-        //   1.3
-      ) {
-        topImages.push(
+      if (post.data.preview.images[0].resolutions[3] && !post.data.is_video) {
+        images.push(
           post.data.preview.images[0].resolutions[3].url.replace(/amp;/gi, '')
         );
       }
@@ -126,70 +100,33 @@ function fetchImages(subredditName) {
     container.classList.add('hidden');
     startMeditation('hot');
   });
-
-  sortByTop.addEventListener('click', function () {
-    sorting.classList.add('hidden');
-    container.classList.add('hidden');
-    startMeditation('top');
-  });
 }
 
 function downloadMoreImages(s) {
-  var xhr;
-  if (s === 'hot') {
-    xhr = new XMLHttpRequest();
-    xhr.open(
-      'GET',
-      `https://www.reddit.com/r/${subredditName}.json?after=${afterHot}`
-    );
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onload = function () {
-      // console.log(xhr.response);
-      var responseObj = xhr.response.data.children;
-      responseObj.forEach(function (post) {
-        if (post.data.preview && !post.data.is_video) {
-          // console.log(post.data.preview.images[0].resolutions[3].url);
-          hotImages.push(
-            post.data.preview.images[0].resolutions[3].url.replace(/amp;/gi, '')
-          );
-        }
-      });
-    };
-  } else if (s === 'top') {
-    xhr = new XMLHttpRequest();
-    xhr.open(
-      'GET',
-      `https://www.reddit.com/r/${subredditName}/top.json?t=all&after=${afterTop}`
-    );
-    xhr.responseType = 'json';
-    xhr.send();
-    xhr.onload = function () {
-      // console.log(xhr.response);
-      var responseObj = xhr.response.data.children;
-      responseObj.forEach(function (post) {
-        if (post.data.preview && !post.data.is_video) {
-          // console.log(post.data.preview.images[0].resolutions[3].url);
-          topImages.push(
-            post.data.preview.images[0].resolutions[3].url.replace(/amp;/gi, '')
-          );
-        }
-      });
-    };
-  }
+  // console.log('More');
+  var moreXhr = new XMLHttpRequest();
+  moreXhr.open(
+    'GET',
+    `https://www.reddit.com/r/${subredditName}.json?after=${afterHot}`
+  );
+  moreXhr.responseType = 'json';
+  moreXhr.send();
+  moreXhr.onload = function () {
+    var moreResponseObj = moreXhr.response.data.children;
+    afterHot = moreXhr.response.data.after;
+    moreResponseObj.forEach(function (post) {
+      if (post.data.preview.images[0].resolutions[3] && !post.data.is_video) {
+        images.push(
+          post.data.preview.images[0].resolutions[3].url.replace(/amp;/gi, '')
+        );
+      }
+    });
+  };
 }
 
 function startMeditation(s) {
   close.classList.remove('hidden');
   // var speedInMs = 6000;
-
-  if (s === 'hot') {
-    images = hotImages;
-    topImages = [];
-  } else if (s === 'top') {
-    images = topImages;
-    hotImages = [];
-  }
 
   close.addEventListener('click', function () {
     document.location.reload(true);
@@ -200,8 +137,20 @@ function startMeditation(s) {
   image.classList.remove('hidden');
 
   setInterval(function () {
-    image.src = images[i];
+    var img = new Image();
+    img.src = images[i];
     i++;
+    img.onload = function () {
+      // console.log('OK');
+      image.src = images[i];
+    };
+    img.onerror = function () {
+      // console.log('Not OK');
+      i++;
+    };
+
+    // image.src = images[i];
+    // i++;
 
     if (i > images.length - 2) {
       downloadMoreImages(s);
@@ -210,3 +159,8 @@ function startMeditation(s) {
 }
 
 chooseSubreddit();
+
+// xhr.open(
+//   'GET',
+//   `https://www.reddit.com/r/${subredditName}/top.json?t=all&after=${afterTop}`
+// );

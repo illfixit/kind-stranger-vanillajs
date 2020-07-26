c4f6936b76d1ad467e7ca38e43abc9b1137f5172
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-26 14:31:12 +0300

    Added Sorting Types & Everything works great!

--- script.js
+++ script.js
@@ -6,18 +6,37 @@ var subredditContainer = document.getElementById('subreddit-container');
 var subreddit = document.getElementById('subreddit');
 var sorting = document.getElementById('sorting');
 var sortByHot = document.getElementById('sort-by-hot');
-var sortByTop = document.getElementById('sort-by-top');
+var sortByNew = document.getElementById('sort-by-new');
+var sortByTopDay = document.getElementById('sort-by-top-day');
+var sortByTopWeek = document.getElementById('sort-by-top-week');
+var sortByTopMonth = document.getElementById('sort-by-top-month');
+var sortByTopYear = document.getElementById('sort-by-top-year');
+var sortByTopAll = document.getElementById('sort-by-top-all');
 var ok = document.getElementById('ok');
 var buttons = document.getElementById('buttons');
 
 var close = document.getElementById('close');
 
+// var sortBy = 'HOT';
 var subredditName = '';
-var hotImages = [];
-var topImages = [];
 var images = [];
-var afterTop = '';
-var afterHot = '';
+var after = '';
+var sortedUrl 
+
+// var currentUrl = window.location.href;
+// var params = currentUrl.split('?')[1]
+// // console.log(params)
+// if(params) {
+// 	subredditName = params;
+//     subredditName = subredditName.trim();
+//     subreddit.value = '';
+//     subreddit.classList.add('hidden');
+//     buttons.classList.add('hidden');
+//     subredditContainer.classList.add('hidden');
+//     sorting.classList.remove('hidden');
+//     fetchImages(subredditName);
+// }  
+
 
 function chooseSubreddit() {
   subreddit.addEventListener('keydown', function (e) {
@@ -30,7 +49,7 @@ function chooseSubreddit() {
         subredditContainer.classList.add('hidden');
         sorting.classList.remove('hidden');
 
-        fetchImages(subredditName);
+    chooseTypeOfSort(subredditName);
       }
     }
   });
@@ -44,7 +63,7 @@ function chooseSubreddit() {
       subredditContainer.classList.add('hidden');
       sorting.classList.remove('hidden');
 
-      fetchImages(subredditName);
+    chooseTypeOfSort(subredditName);
     }
   });
 
@@ -56,64 +75,115 @@ function chooseSubreddit() {
     buttons.classList.add('hidden');
     subredditContainer.classList.add('hidden');
     sorting.classList.remove('hidden');
-    // alert(subredditName);
 
-    fetchImages(subredditName);
+    chooseTypeOfSort(subredditName);
   });
 
   subreddit.addEventListener('input', function (e) {});
 }
 
-function fetchImages(subredditName) {
-  // Promise Hot
-  // var promise = new Promise(function(resolve, reject) {
-  //   var xhrHot = new XMLHttpRequest();
-  //   xhrHot.open(
-  //     'GET',
-  //     `https://www.reddit.com/r/${subredditName}.json?limit=100`
-  //   );
-  //   xhrHot.responseType = 'json';
-  //   xhrHot.send();
-  // })
 
-  // Sort by Hot
-  var xhrHot = new XMLHttpRequest();
-  xhrHot.open('GET', `https://www.reddit.com/r/${subredditName}.json`);
-  xhrHot.responseType = 'json';
-  xhrHot.send();
-  xhrHot.onload = function () {
-    var responseObj = xhrHot.response.data.children;
-    afterHot = xhrHot.response.data.after;
+function chooseTypeOfSort(subredditName) {
+
+	  // Event Listeners
+ 	 sortByHot.addEventListener('click', function () {
+    	sorting.classList.add('hidden');
+    	container.classList.add('hidden');
+    	fetchImages(subredditName, 'HOT');
+  	});
+
+ 	 sortByNew.addEventListener('click', function () {
+    	sorting.classList.add('hidden');
+    	container.classList.add('hidden');
+    	fetchImages(subredditName, 'NEW');
+  	});
+
+ 	 sortByTopDay.addEventListener('click', function () {
+    	sorting.classList.add('hidden');
+    	container.classList.add('hidden');
+    	fetchImages(subredditName, 'TOPOFTHEDAY');
+  	});
+
+ 	 sortByTopWeek.addEventListener('click', function () {
+    	sorting.classList.add('hidden');
+    	container.classList.add('hidden');
+    	fetchImages(subredditName, 'TOPOFTHEWEEK');
+  	}); 	 
+
+ 	 sortByTopMonth.addEventListener('click', function () {
+    	sorting.classList.add('hidden');
+    	container.classList.add('hidden');
+    	fetchImages(subredditName, 'TOPOFTHEMONTH');
+  	}); 	 
+
+ 	 sortByTopYear.addEventListener('click', function () {
+    	sorting.classList.add('hidden');
+    	container.classList.add('hidden');
+    	fetchImages(subredditName, 'TOPOFTHEYEAR');
+  	}); 	 
+
+ 	 sortByTopAll.addEventListener('click', function () {
+    	sorting.classList.add('hidden');
+    	container.classList.add('hidden');
+    	fetchImages(subredditName, 'TOPOFALLTIME');
+  	}); 	 
+}
+
+
+
+function fetchImages(subredditName, sortBy) {
+
+  sortedUrl = `https://www.reddit.com/r/${subredditName}.json`
+
+  switch(sortBy) {
+    case 'HOT' : sortedUrl = `https://www.reddit.com/r/${subredditName}.json`
+    break;
+    case 'NEW' : sortedUrl = `https://www.reddit.com/r/${subredditName}/new.json`
+    break;
+    case 'TOPOFTHEDAY' : sortedUrl = `https://www.reddit.com/r/${subredditName}/top.json?t=day`
+    break;
+    case 'TOPOFTHEWEEK' : sortedUrl = `https://www.reddit.com/r/${subredditName}/top.json?t=week`
+    break;
+    case 'TOPOFTHEMONTH' : sortedUrl = `https://www.reddit.com/r/${subredditName}/top.json?t=month`
+    break;
+    case 'TOPOFTHEYEAR' : sortedUrl = `https://www.reddit.com/r/${subredditName}/top.json?t=year`
+    break;
+    case 'TOPOFALLTIME' : sortedUrl = `https://www.reddit.com/r/${subredditName}/top.json?t=all`
+    break;
+  }
+
+  var sortedXhr = new XMLHttpRequest();
+  sortedXhr.open('GET', sortedUrl);
+  sortedXhr.responseType = 'json';
+  sortedXhr.send();
+  sortedXhr.onload = function () {
+    var responseObj = sortedXhr.response.data.children;
+    after = sortedXhr.response.data.after;
 
     responseObj.forEach(function (post) {
-      // console.log(post.data.preview);
       if (post.data.preview.images[0].resolutions[3] && !post.data.is_video) {
         images.push(
           post.data.preview.images[0].resolutions[3].url.replace(/amp;/gi, '')
         );
       }
     });
-  };
 
-  sortByHot.addEventListener('click', function () {
-    sorting.classList.add('hidden');
-    container.classList.add('hidden');
-    startMeditation('hot');
-  });
+    startMeditation(sortedUrl)
+  };
 }
 
-function downloadMoreImages(s) {
+function downloadMoreImages(url) {
   // console.log('More');
   var moreXhr = new XMLHttpRequest();
   moreXhr.open(
     'GET',
-    `https://www.reddit.com/r/${subredditName}.json?after=${afterHot}`
+    `${url}?after=${after}`
   );
   moreXhr.responseType = 'json';
   moreXhr.send();
   moreXhr.onload = function () {
     var moreResponseObj = moreXhr.response.data.children;
-    afterHot = moreXhr.response.data.after;
+    after = moreXhr.response.data.after;
     moreResponseObj.forEach(function (post) {
       if (post.data.preview.images[0].resolutions[3] && !post.data.is_video) {
         images.push(
@@ -124,7 +194,7 @@ function downloadMoreImages(s) {
   };
 }
 
-function startMeditation(s) {
+function startMeditation(url) {
   close.classList.remove('hidden');
   // var speedInMs = 6000;
 
@@ -153,7 +223,7 @@ function startMeditation(s) {
     // i++;
 
     if (i > images.length - 2) {
-      downloadMoreImages(s);
+      downloadMoreImages(url);
     }
   }, 5000);
 }
@@ -163,4 +233,24 @@ chooseSubreddit();
 // xhr.open(
 //   'GET',
 //   `https://www.reddit.com/r/${subredditName}/top.json?t=all&after=${afterTop}`
+// );
+
+// xhr.open(
+//   'GET',
+//   `https://www.reddit.com/r/${subredditName}/top.json?t=day&after=${afterTop}`
+// );
+
+// xhr.open(
+//   'GET',
+//   `https://www.reddit.com/r/${subredditName}/top.json?t=week&after=${afterTop}`
+// );
+
+// xhr.open(
+//   'GET',
+//   `https://www.reddit.com/r/${subredditName}/top.json?t=month&after=${afterTop}`
+// );
+
+// xhr.open(
+//   'GET',
+//   `https://www.reddit.com/r/${subredditName}/new.json&after=${afterTop}`
 // );
